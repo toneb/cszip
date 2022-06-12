a3ef21cc9095b299f090e5c96f2a250e14d07c44
toneb <tone.bone@protonmail.com>
2022-06-12 11:33:20 +0200

    Vite lib build config.

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+﻿const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'cszip'
+        }
+    }
+})
